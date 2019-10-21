1b5a0072eb52470110c74e55760c7723eee38ba0
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-21 14:24:17 +0200

    change fetchPokemons to recive 25 first pokemons not only 5

--- src/services/fetchPokemons.js
+++ src/services/fetchPokemons.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'http://pokeapi.salestock.net/api/v2/pokemon?offset=0&limit=5';
+const ENDPOINT = 'http://pokeapi.salestock.net/api/v2/pokemon?offset=0&limit=25';
 
 const fetchPokemons = () => fetch(ENDPOINT).then(response => response.json());
 
