f947edf7e848e56c0cd30b2027498568c4af3c0e
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-23 16:34:48 +0200

    install router and change endpoint

--- src/services/fetchPokemons.js
+++ src/services/fetchPokemons.js
@@ -1,4 +1,4 @@
-const ENDPOINT = 'http://pokeapi.salestock.net/api/v2/pokemon?offset=0&limit=25';
+const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=25';
 
 const fetchPokemons = () => fetch(ENDPOINT).then(response => response.json());
 
