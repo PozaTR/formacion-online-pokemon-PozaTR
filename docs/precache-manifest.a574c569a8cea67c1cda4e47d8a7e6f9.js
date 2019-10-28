27fe9fd0a5d05c3c348497cf7aa6f3cd4a1665b2
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-28 13:27:47 +0100

    create docs

---
+++ docs/precache-manifest.a574c569a8cea67c1cda4e47d8a7e6f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ffd93371643a663c67876a2ae81bb8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ea3b73e071e1083dc659",
+    "url": "./static/css/main.501b92bf.chunk.css"
+  },
+  {
+    "revision": "2e0129f74aa61daa06c5",
+    "url": "./static/js/2.0994654a.chunk.js"
+  },
+  {
+    "revision": "ea3b73e071e1083dc659",
+    "url": "./static/js/main.57a49dab.chunk.js"
+  },
+  {
+    "revision": "3cfe1a54e88b7a379f5b",
+    "url": "./static/js/runtime-main.6b214a37.js"
+  },
+  {
+    "revision": "570fcab842fdd7d199a526f2a1e5f0f6",
+    "url": "./static/media/Pokemon-Logo.570fcab8.png"
+  }
+]);
