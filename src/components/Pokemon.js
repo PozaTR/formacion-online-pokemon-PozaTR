1311969f98a633eedacdbda37f8ff4193d024bdf
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-10-29 12:11:21 +0100

    add proptypes in PokemonTypesBackground fix IsLoading props and change pokemon class component to dummy component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,42 +3,42 @@ import PropTypes from 'prop-types';
 import PokemonTypesBackground from './PokemonTypesBackground';
 import '../styles/components/pokemon.scss';
 
-class Pokemon extends React.Component {
-  render() {
-    const { pokemon, isLoading } = this.props;
-    return(
-      <div className="pokemon">
-        { !isLoading
-          ? <React.Fragment>
-              <PokemonTypesBackground  pokemonTypes={pokemon.types} showType/>
-              <div className="pokemon__information" >
-                <div className="pokemon__information__container">
-                  <img className="pokemon__image" src={pokemon.sprites.front_shiny} alt={pokemon.name}/>
-                  <p className="pokemon__name">{pokemon.name}</p>
-                  {pokemon.evolves_from_species ? (<p className="pokemon__evolution" >{pokemon.evolves_from_species.name}</p>) : ''}
-                  <ul className="pokemon__types">
-                    {pokemon.types.map((type, index) => 
-                      <li className="pokemon__type" key={`type-${index}`}>{type.type.name}</li>
-                    )}
-                  </ul>
-                </div>
-                <p className="pokemon__number" >ID/{pokemon.id}</p>
-              </div>
-            </React.Fragment> 
-          : <React.Fragment>
+const Pokemon = (props) => {
+  const { pokemon, isLoading } = props;
+
+  return(
+    <div className="pokemon">
+      { !isLoading
+        ? <React.Fragment>
+            <PokemonTypesBackground  pokemonTypes={pokemon.types} showType/>
+            <div className="pokemon__information" >
               <div className="pokemon__information__container">
+                <img className="pokemon__image" src={pokemon.sprites.front_shiny} alt={pokemon.name}/>
                 <p className="pokemon__name">{pokemon.name}</p>
-                <div className="pokemon__loader"></div>
+                {pokemon.evolves_from_species ? (<p className="pokemon__evolution" >{pokemon.evolves_from_species.name}</p>) : ''}
+                <ul className="pokemon__types">
+                  {pokemon.types.map((type, index) => 
+                    <li className="pokemon__type" key={`type-${index}`}>{type.type.name}</li>
+                  )}
+                </ul>
               </div>
-            </React.Fragment>
-        }
-      </div>
-    )
-  }
+              <p className="pokemon__number" >ID/{pokemon.id}</p>
+            </div>
+          </React.Fragment> 
+        : <React.Fragment>
+            <div className="pokemon__information__container">
+              <p className="pokemon__name">{pokemon.name}</p>
+              <div className="pokemon__loader"></div>
+            </div>
+          </React.Fragment>
+      }
+    </div>
+  )
 }
 
 Pokemon.propTypes = {
-  pokemon: PropTypes.object.isRequired
+  pokemon: PropTypes.object.isRequired,
+  isLoading: PropTypes.bool.isRequired
 }
 
 export default Pokemon; 
--- src/components/PokemonTypesBackground.js
+++ src/components/PokemonTypesBackground.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../styles/components/pokemonTypesBackground.scss';
 
 const pokemonTypesBackground = (props) => {
@@ -10,6 +11,10 @@ const pokemonTypesBackground = (props) => {
     )}
     </ul>
   )
+}
+
+pokemonTypesBackground.propTypes = {
+  pokemonTypes: PropTypes.arrayOf(PropTypes.object).isRequired,
 }
 
 export default pokemonTypesBackground;
