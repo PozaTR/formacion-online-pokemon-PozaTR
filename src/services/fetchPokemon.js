8d8f36043ae0f7b6e377966a9b83b7f0ba40479b
Rocío Poza <rocio.poza.tejeda@gmail.com>
2019-11-03 10:43:46 +0100

    change fetch pokemon to control error catch

--- src/services/fetchPokemon.js
+++ src/services/fetchPokemon.js
@@ -4,17 +4,38 @@ const ENDPOINT = 'https://pokeapi.co/api/v2/pokemon/';
 
 const _fetchPokemon = (pokemonId) => fetch(ENDPOINT+pokemonId).then(response => response.json());
 
-const fetchPokemon = (pokemonId) => Promise.all([
-    _fetchPokemon(pokemonId),
-    fetchSpecie(pokemonId)
-  ])
-  .then((responses) => {
-    const pokemonData = responses[0]
-    const specieData = responses[1]
-    return {
-      ...pokemonData,
+const fetchPokemon = async (pokemonId) => {
+  let pokemon = {}
+  try{
+    const pokemonData = await _fetchPokemon(pokemonId)
+    pokemon = {
+      ...pokemonData
+    }
+
+    const specieData = await fetchSpecie(pokemonId)
+    pokemon = {
+      ...pokemon,
       ...specieData
     }
-  })
+  } catch (err) {
+    return pokemon
+  }
+  return pokemon
+}
 
 export { fetchPokemon };
+
+//First attempt using Promise.all
+//
+// const fetchPokemon = (pokemonId) => Promise.all([
+//     _fetchPokemon(pokemonId),
+//     fetchSpecie(pokemonId)
+//   ])
+//   .then((responses) => {
+//     const pokemonData = responses[0]
+//     const specieData = responses[1]
+//     return {
+//       ...pokemonData,
+//       ...specieData
+//     }
+//   })
